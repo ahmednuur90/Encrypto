327d73338aaf9cf2fd98a999359830014cfa0f80
ahmed <ahmednuur90@gmail.com>
2021-05-09 01:43:37 -0400

    Added two starter files, Main.py and Encryp_Decryp.py

---
+++ Encryp_Decryp.py
@@ -0,0 +1,13 @@
+class Encryp_Decryp:
+    def __init__(self):
+        pass
+
+    def menu(self):
+        print("Please pick from the following options!")
+        print("1. Encrypt a file")
+        print("2. Decrypt a file (pick if you have a file you want to decrypt)\n")
+        num = int(input())
+        self.choice(num)
+
+    def choice(self, num):
+        pass
---
+++ Main.py
@@ -0,0 +1,10 @@
+from Encryp_Decryp import Encryp_Decryp
+
+
+def main():
+    encrypt = Encryp_Decryp()
+    encrypt.menu()
+
+
+if __name__ == '__main__':
+    main()
